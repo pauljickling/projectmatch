3325a3498956476516a3a69656aa79748dcd94f5
Paul Jickling <pjickling@gmail.com>
2018-11-08 10:24:41 -0800

    Changed database engine from SQLite to PostgreSQL

--- projectmatch/settings.py
+++ projectmatch/settings.py
@@ -76,10 +76,12 @@ WSGI_APPLICATION = 'projectmatch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'paulj',
+        'USER': 'paulj',
     }
 }
 
