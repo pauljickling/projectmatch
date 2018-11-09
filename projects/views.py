83f4ca2a8206671b7f23501ca91124558a496efb
Paul Jickling <pjickling@gmail.com>
2018-11-09 10:24:31 -0800

    Initial url and view config

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpRequest
+from django.template import Context, Template
+from django.template.loader import get_template
 
-# Create your views here.
+def index(request):
+    return HttpResponse('<h1>hello world!</h1>')
