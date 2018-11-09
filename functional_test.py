8f55ea7df6b5ab2c4d6ea9a26f7c5a19418f4a4a
Paul Jickling <pjickling@gmail.com>
2018-11-09 11:41:30 -0800

    Updated first successful test

--- functional_test.py
+++ functional_test.py
@@ -3,4 +3,6 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+assert 'Django' in browser.title, "Browser title was " + browser.title
+
+browser.quit()
