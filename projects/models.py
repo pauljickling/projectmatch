6e0cde50b22bd0bb67c6e631f3bf637724db126c
Paul Jickling <pjickling@gmail.com>
2018-11-11 15:04:55 -0800

    Init project model added

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=454)
+    summary = models.CharField(max_length=454)
+    pending_tasks = models.CharField(max_length=454)
+    progress_made = models.CharField(max_length=454)
+    additional_info = models.CharField(max_length=454)
+    slack_channel = models.CharField(max_length=200)
