efc1d26081d9d18aeec83d5089edf6bf6d83ef0c
Paul Jickling <pjickling@gmail.com>
2018-11-14 20:08:28 -0800

    View converted to raw display of project list

--- projects/views.py
+++ projects/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
 from django.template import Context, Template
 from django.template.loader import get_template
+from .models import Project
 
 def index(request):
-    tag = "<h1>hello world</h1>"
+    projects = Project.objects.all()
     context = {
-        "hello": tag,
+        "hello": projects,
     }
     return render(request, 'projects/index.html', context)
